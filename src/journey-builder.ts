4318bb7ebe99a2f43f63ed2f2b3c54085fa1ac0e
V1rtus9 <v.botnar@mail.ru>
2021-07-11 00:48:06 +0200

    journey builder update

--- src/journey-builder.ts
+++ src/journey-builder.ts
@@ -1,4 +1,4 @@
-import RestClient from "./clients/rest";
+import RestClient from './clients/rest';
 
 export class Journey {
 
@@ -21,44 +21,36 @@ export class Journey {
     }
 
     /**
-     * @todo Needs to be tested
+     * Returns raw journey object that comes from Marketing Cloud
      */
+    public get raw() {
+        return this._rawData;
+    }
+
     async stop() {
         const url = `/interaction/v1/interactions/stop/${this.id}?versionNumber=${this.version}`;
         const result = await this._rest.post(url);
         return result;
     }
 
-    /**
-     * @todo Needs to be tested
-     */
     async pause() {
         const url = `/interaction/v1/interactions/pause/${this.id}?versionNumber=${this.version}`;
         const result = await this._rest.post(url);
         return result;
     }
 
-    /**
-     * @todo Needs to be tested
-     */
-    async update(data: any) {
-        const url = `/interaction/v1/interactions/?versionNumber=${this.version}`;
-        const result = await this._rest.put(url, data);
-        return result.items ? result.items : [];
-    }
-    
-    /**
-     * @todo Needs to be tested
-     */
     async resume() {
         const url = `/interaction/v1/interactions/resume/${this.id}?versionNumber=${this.version}`;
         const result = await this._rest.post(url);
         return result;
     }
 
-    /**
-     * @todo Needs to be tested
-     */   
+    async update(data: any) {
+        const url = `/interaction/v1/interactions/?versionNumber=${this.version}`;
+        const result = await this._rest.put(url, data);
+        return result;
+    }
+
     async publish() {
         const url = `/interaction/v1/interactions/publishAsync/${this.id}?versionNumber=${this.version}`;
         const result = await this._rest.post(url);
@@ -66,17 +58,13 @@ export class Journey {
     }
 
     async newVersion() {
-        this._rawData.status = 'Draft';
 
         delete this._rawData.id
         delete this._rawData.version;
         delete this._rawData.definitionId;
 
+        this._rawData.status = 'Draft';
         const result = await this._rest.post(`/interaction/v1/interactions/`, this._rawData);
-
-        if(!result){
-            throw new Error(`Could not create new verions of journey "${this.id}"`);
-        }
 
         this._rawData = result;
         this.version = result.version;
@@ -90,20 +78,155 @@ export class JourneyBuilder {
         this._rest = rest;
     }
 
-    async getJourney(id:string, versionNumber: number): Promise<Journey | null>{
-        const result = 
+    public async getJourney(id:string, versionNumber?: number): Promise<Journey | null> {
+        /**
+         * 
+            {
+                id: '03ca2399-818f-44fa-9baa-106a1d3d0728',
+                key: '7f931f23-2516-9a36-e5d6-7f82b098263f',
+                name: 'demo 01022020',
+                lastPublishedDate: '0001-01-01T00:00:00',
+                description: '',
+                version: 1,
+                workflowApiVersion: 1,
+                createdDate: '2021-02-01T03:30:31.907',
+                modifiedDate: '2021-02-01T03:30:31.907',
+                activities: [
+                {
+                    id: '71dfb502-93c4-49e1-a608-573e97c26378',
+                    key: 'WAITBYDURATION-1',
+                    name: '',
+                    description: '',
+                    type: 'WAIT',
+                    outcomes: [Array],
+                    arguments: {},
+                    configurationArguments: [Object],
+                    metaData: [Object],
+                    schema: [Object]
+                }
+                ],
+                triggers: [
+                {
+                    id: '27502fa4-37c3-4cbc-89a3-a414fd3fd8d8',
+                    key: 'TRIGGER',
+                    name: 'TRIGGER',
+                    description: '',
+                    type: 'AutomationAudience',
+                    outcomes: [],
+                    arguments: {},
+                    configurationArguments: {},
+                    metaData: [Object]
+                }
+                ],
+                goals: [
+                {
+                    id: '6c345b85-c78e-4b36-b74b-979628c88929',
+                    key: 'GOAL',
+                    name: 'GOAL',
+                    description: '',
+                    type: 'Event',
+                    outcomes: [],
+                    arguments: {},
+                    configurationArguments: [Object],
+                    metaData: [Object],
+                    schema: [Object]
+                }
+                ],
+                exits: [],
+                notifiers: [],
+                stats: {
+                currentPopulation: 0,
+                cumulativePopulation: 0,
+                metGoal: 0,
+                metExitCriteria: 0,
+                goalPerformance: 0
+                },
+                entryMode: 'NotSet',
+                definitionType: 'Multistep',
+                channel: '',
+                defaults: {
+                email: [
+                    '{{Event.DEAudience-140eb541-b240-9afd-fc0f-d3428653dec4."email"}}'
+                ],
+                properties: { analyticsTracking: [Object] }
+                },
+                metaData: {},
+                executionMode: 'Production',
+                categoryId: 40213,
+                status: 'Draft',
+                definitionId: '03ca2399-818f-44fa-9baa-106a1d3d0728',
+                scheduledStatus: 'Draft'
+            }
+         * 
+         */
+        const response = 
             await this._rest.get(
-                `/interaction/v1/interactions/${id}?versionNumber=${versionNumber}`
+                `/interaction/v1/interactions/${id}${versionNumber ? `?versionNumber=${versionNumber}` : ''}`
             );
         
-        return result ? new Journey(result, this._rest) : null; 
+        return response ? new Journey(response, this._rest) : null; 
     }
 
-    async getJourneys(page: number = 1): Promise<Array<Journey>>{
+    public async getJourneys(page: number = 1): Promise<Array<Journey>>{
         const result = await this._rest.get(`/interaction/v1/interactions?$page=${page}`);
+
+        /**
+         * 
+            {
+                count: 1,
+                page: 1,
+                pageSize: 50,
+                links: {},
+                items: [
+                    {
+                        id: '38422c30-0420-46a1-ab69-3327ea35a3a5',
+                        key: '9f7ee1da-119b-294c-729d-1dada7da0d62',
+                        name: 'test journey',
+                        lastPublishedDate: '0001-01-01T00:00:00',
+                        description: '',
+                        version: 1,
+                        workflowApiVersion: 1,
+                        createdDate: '2019-07-22T15:54:48.547',
+                        modifiedDate: '2019-07-22T15:55:02.07',
+                        goals: [],
+                        exits: [],
+                        stats: {
+                            currentPopulation: 0,
+                            cumulativePopulation: 0,
+                            metGoal: 0,
+                            metExitCriteria: 0,
+                            goalPerformance: 0
+                        },
+                        entryMode: 'NotSet',
+                        defaults: {
+                            email: [
+                                '{{Event.APIEvent-0dbe46de-74f9-a309-7778-298c0a565f93."Email"}}'
+                            ],
+                            properties: {
+                                analyticsTracking: {
+                                    enabled: false,
+                                    analyticsType: 'google',
+                                    urlDomainsToTrack: []
+                                }
+                            }
+                        },
+                        metaData: {},
+                        executionMode: 'Production',
+                        categoryId: 731761,
+                        status: 'Draft',
+                        definitionId: '38422c30-0420-46a1-ab69-3327ea35a3a5'
+                    }
+                ]
+            }
+         */
         return result.items ? result.items.map((x: any) => {
             return new Journey(x, this._rest);
         }) : [];
+    }
+
+    public async getJourneysCount(): Promise<number> {
+        const response = await this._rest.get(`/interaction/v1/interactions`);
+        return response.count;
     }
 }
 
--- tests/journey-builder.test.ts
+++ tests/journey-builder.test.ts
@@ -1,25 +1,33 @@
 import instance from './core/instance';
 
-const builder = instance.getJourneyBuilder();
+describe('Journey Builder', () => {
+    const builder = instance.getJourneyBuilder();
 
+    test('Get journey', async () => {
+        const journey = 
+            await builder.getJourney('03ca2399-818f-44fa-9baa-106a1d3d0728', 1);
+    
+        expect(journey).not.toBeNull();
+    });
 
-test('Get journeys', async () => {
-    const journeys = 
-        await builder.getJourneys();
+    test('Get journeys', async () => {
+        const journeys = 
+            await builder.getJourneys();
+    
+        expect(journeys.length).toBeGreaterThan(0);
+    });
 
-    expect(journeys.length).toBeGreaterThan(0);
-});
-
-test('Get journey', async () => {
-    const journey = 
-        await builder.getJourney('03ca2399-818f-44fa-9baa-106a1d3d0728', 1);
-
-    expect(journey).not.toBeNull();
-});
-
-test('Create new version', async () => {
-    const journey = 
-        await builder.getJourney('03ca2399-818f-44fa-9baa-106a1d3d0728', 1);
-
-    await journey?.newVersion();
-});
+    test('Get journeys count', async () => {
+        const count = 
+            await builder.getJourneysCount();
+    
+        expect(count).toBeGreaterThan(0);
+    });
+    
+    test('Create new version', async () => {
+        const journey = 
+            await builder.getJourney('03ca2399-818f-44fa-9baa-106a1d3d0728', 1);
+    
+        await journey?.newVersion();
+    });
+})
