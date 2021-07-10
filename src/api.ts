6e584c4dab61315804b4be379df3f8d51226ebd4
V1rtus9 <v.botnar@mail.ru>
2021-07-10 23:21:50 +0200

    api update

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,13 @@
 const ET_Client = require('sfmc-fuelsdk-node');
 
 import {DataExtension, DataExtensionRow} from './data-extension';
-import {Journey, JourneyBuilder} from './journey-builder';
+import {JourneyBuilder} from './journey-builder';
 
-import {ISfmcApiCache} from './interfaces/cache';
-import {IClient} from './interfaces/client';
-import {IApiOptions} from './interfaces/options';
+import {IApiClientOptions} from './interfaces/options';
+
+import SoapClient from './clients/soap';
+import RestClient from './clients/rest';
+import { ContactBuilder } from './contact-builder';
 
 export {
     SfmcApi,
@@ -14,11 +16,10 @@ export {
 
 class SfmcApi {
 
-    private _client: IClient;
-    private _restClient: any;
-    private _soapClient: any;
+    private _restClient: RestClient;
+    private _soapClient: SoapClient;
     
-    constructor({clientId, clientSecret, subdomain, accountId}: IApiOptions){
+    constructor({clientId, clientSecret, subdomain, accountId}: IApiClientOptions){
         if(!clientId) throw new Error('Client ID is missing!');
         if(!subdomain) throw new Error('Subdomain is missing!');
         if(!accountId) throw new Error('Account ID is missing!');
@@ -38,80 +39,15 @@ class SfmcApi {
                 }
         });
 
-        this._restClient = instance.RestClient;
-        this._soapClient = instance.SoapClient;
-
-        this._client = {
-            get: this.get,
-            put: this.put,
-            post: this.post,
-            delete: this.delete,
-            retrieve: this.retrieve
-        }
+        this._restClient = new RestClient(instance.RestClient);
+        this._soapClient = new SoapClient(instance.SoapClient);
     }
 
-    public get = async (uri: string) => {
-        return (await this._restClient.get({
-            uri,
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            json: true
-        })).body;
-    }
-
-    public post = async (uri:string, body:any) => {
-        return (await this._restClient.post({
-            uri,
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            json: true,
-            body
-        })).body;
-    }
-
-    public put = async (uri: string, body: any) => {
-        return (await this._restClient.put({
-            uri,
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            json: true,
-            body
-        })).body;
-    }
-
-    public delete = async (uri: string) => {
-        return (await this._restClient.delete({
-            uri,
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            json: true,
-        })).body;
-    }
-
-    public retrieve = async (type: string, props: any[], filter: any) => {
-        return new Promise((resolve, reject) => {
-            this._soapClient.retrieve(type, props, {
-                filter
-            }, (err: any, response: any) => {
-                if(err) reject(err);
-                
-                const {Results, OverallStatus} = response.body;
-                OverallStatus === 'OK' ?
-                    resolve(Results) :
-                    reject(JSON.stringify(response.body));
-            })
-        })
+    getContactBuilder(): ContactBuilder {
+        return new ContactBuilder(this._restClient, this._soapClient);
     }
 
     getJourneyBuilder(): JourneyBuilder {
-        return new JourneyBuilder(this._client)
-    }
-
-    getDataExtension(key: string): DataExtension {
-        return new DataExtension(key, this._client);
+        return new JourneyBuilder(this._restClient);
     }
 }
