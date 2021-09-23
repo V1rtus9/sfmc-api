2c1b385dd16444445dc4585b85a8b11bbe7e589f
V1rtus9 <v.botnar@mail.ru>
2021-09-23 14:42:07 +0200

    soap error interface

--- src/clients/soap.ts
+++ src/clients/soap.ts
@@ -1,3 +1,14 @@
+interface ISoapError extends Error {
+    requestId?: string;
+    results?: Array<{
+        StatusCode: string;
+        StatusMessage: string;
+        OrdinalID: string;
+        ErrorCode: string;
+        NewID: string;
+    }>;
+}
+
 export default class SoapClient {
     private _instance: any;
 
@@ -7,16 +18,16 @@ export default class SoapClient {
 
     public create = (type: string, props: {[key: string]: any}): Promise<any> => {
         return new Promise((resolve, reject) => {
-            this._instance.create(type, props, {}, (err: any, response: any) => {
-                return err ? reject(err.message) : resolve(response.body);
+            this._instance.create(type, props, {}, (err: ISoapError, response: any) => {
+                return err ? reject(err) : resolve(response.body);
             })
         })
     }
 
     public retrieve = (type: string, props: Array<string>, options: any): Promise<any> => {
         return new Promise((resolve, reject) => {
-            this._instance.retrieve(type, props, options, (err: any, response: any) => {
-                return err ? reject(err.message) : resolve(response.body);
+            this._instance.retrieve(type, props, options, (err: ISoapError, response: any) => {
+                return err ? reject(err) : resolve(response.body);
             })
         })
     }
@@ -27,8 +38,8 @@ export default class SoapClient {
 
     public delete = (type: string, props: {[key: string]: any}): Promise<any> => {
         return new Promise((resolve, reject) => {
-            this._instance.delete(type, props, {}, (err: any, response: any) => {
-                return err ? reject(err.message) : resolve(response.body);
+            this._instance.delete(type, props, {}, (err: ISoapError, response: any) => {
+                return err ? reject(err) : resolve(response.body);
             })
         })
     }
