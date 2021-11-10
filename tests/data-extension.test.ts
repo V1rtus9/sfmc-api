58164838f113774fd99b231766418b073db9c4ad
V1rtus9 <v.botnar@mail.ru>
2021-11-11 00:17:54 +0100

    data-extension test
    - filter and get only defined fields

--- tests/data-extension.test.ts
+++ tests/data-extension.test.ts
@@ -48,12 +48,12 @@ describe('DataExtension', () => {
                 }
             ]
         });
-    
+
         expect(de).toBeDefined();
     });
-    
+
     test('Get name', async () => {
-        const name = await de.name();
+        const name = await de.getName();
         expect(name).toBe(dataExtensionName);
     });
 
@@ -62,63 +62,63 @@ describe('DataExtension', () => {
             new DataExtensionRow({ subscriberKey: '0001'}, {
                 age: '15',
                 name: 'Alex',
-                email: 'alex@hotmail.ee', 
+                email: 'alex@hotmail.ee',
                 createdAt: new Date().toISOString()
             }),
             new DataExtensionRow({ subscriberKey: '0002'}, {
                 age: '48',
-                name: 'John', 
-                email: 'john@hotmail.ee', 
+                name: 'John',
+                email: 'john@hotmail.ee',
                 createdAt: new Date().toISOString()
             }),
             new DataExtensionRow({ subscriberKey: '0003'}, {
                 age: '72',
-                name: 'Jonathan', 
-                email: 'jonathan@hotmail.ee', 
+                name: 'Jonathan',
+                email: 'jonathan@hotmail.ee',
                 createdAt: new Date().toISOString()
             }),
             new DataExtensionRow({ subscriberKey: '0004'}, {
                 age: '25',
-                name: 'Nadia', 
-                email: 'nadia@hotmail.ee', 
+                name: 'Nadia',
+                email: 'nadia@hotmail.ee',
                 createdAt: new Date().toISOString()
             }),
             new DataExtensionRow({ subscriberKey: '0005'}, {
                 age: '30',
-                name: 'Clara', 
-                email: 'clara@hotmail.ee', 
+                name: 'Clara',
+                email: 'clara@hotmail.ee',
                 createdAt: new Date().toISOString()
             })
         ])
     });
-    
+
     test('Get count', async () => {
-        const count = await de.count();
+        const count = await de.getRowsCount();
         expect(count).toEqual(5);
     });
-    
+
     test('Get rows', async () => {
-        const rows = await de.rows();
+        const rows = await de.getRows();
         expect(rows).toHaveLength(5);
     });
 
     test('Get rows (unofficial)', async () => {
-        const rows = await de.rows2();
+        const rows = await de.getRowsUnofficial();
         expect(rows).toHaveLength(5); 
     })
-    
+
     test('Get rows using simple filter', async () => {
-        const rows = await de.rows({filter: {
+        const rows = await de.getRows({filter: {
             leftOperand: 'SubscriberKey',
             operator: 'equals',
             rightOperand: '0001'
         }});
-    
+
         expect(rows).toHaveLength(1);
     });
-    
+
     test('Get rows using complex filter', async () => {
-        const rows = await de.rows({filter: {
+        const rows = await de.getRows({filter: {
             leftOperand: {
                 leftOperand: 'SubscriberKey',
                 operator: 'equals',
@@ -139,12 +139,12 @@ describe('DataExtension', () => {
                 }
             }
         }});
-        
+
         expect(rows).toHaveLength(3);
     });
 
     test('Get rows using more complex filter', async () => {
-        const rows = await de.rows({filter: {
+        const rows = await de.getRows({filter: {
             leftOperand: {
                 leftOperand: {
                     leftOperand: 'Name',
@@ -176,15 +176,28 @@ describe('DataExtension', () => {
 
         expect(rows).toHaveLength(4);
     })
-    
+
     test('Get fields', async () => {
-        const fields = await de.fields();
+        const fields = await de.getFields();
 
         expect(fields).toContain('Age');
         expect(fields).toContain('Name');
         expect(fields).toContain('CreatedAt');
         expect(fields).toContain('SubscriberKey');
     });
+
+    test('Get selected field and filter', async () => {
+        const rows = await de.getRows({
+            fields: ['name'],
+            filter: {
+                leftOperand: 'SubscriberKey',
+                operator: 'equals',
+                rightOperand: '0001'
+            }
+        });
+
+        expect(rows).toHaveLength(1);
+    })
 
     test('Delete', async () => {
         await instance.getContactBuilder().deleteDataExtension(de.key);
