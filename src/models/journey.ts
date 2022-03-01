3ef4db8ac1a973148f6488f2c8e7eceeece80349
V1rtus9 <v.botnar@mail.ru>
2022-03-01 20:12:24 +0100

    struture update

---
+++ src/models/journey.ts
@@ -0,0 +1,14 @@
+export enum JourneyStatus {
+    Draft = 'Draft',
+    Stopped = 'Stopped',
+    Deleted = 'Deleted',
+    Published = 'Published',
+    Unpublished = 'Unpublished',
+    ScheduledToPublish = 'ScheduledToPublish'
+}
+
+export enum JourneyPublishStatus {
+    Error = 'Error',
+    PublishInProcess = 'PublishInProcess',
+    PublishCompleted = 'PublishCompleted'
+}
